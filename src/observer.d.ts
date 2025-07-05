2f3108ab33e5b41cd5c4875e54cb35109601562e
Jun Jie Huang <1juhuang2@gmail.com>
2025-07-05 14:55:36 -0700

    fix: update types

--- src/observer.d.ts
+++ src/observer.d.ts
@@ -1,11 +1,11 @@
-import { Entity, Id, World } from "@rbxts/jecs";
+import { Entity, Id, Query, World } from "@rbxts/jecs";
 
 interface PatchedWorld extends World {
     added<T>(this: PatchedWorld, id: Id<T>, callback: (e: Entity, id: Id, value: T) => void): () => void;
     removed<T>(this: PatchedWorld, id: Id<T>, callback: (e: Entity, id: Id) => void): () => void;
     changed<T>(this: PatchedWorld, id: Id<T>, callback: (e: Entity, id: Id, value: T) => void): () => void
-    observer(this: PatchedWorld, arg: any, callback: (e: Entity) => void): void;
-    monitor(this: PatchedWorld, arg: any, callback: (e: Entity, id: Id) => void): void;
+    observer(this: PatchedWorld, arg: Query<unknown[]>, callback: (e: Entity) => void): void;
+    monitor(this: PatchedWorld, arg: Query<unknown[]>, callback: (e: Entity, id: Id) => void): void;
 }
 
 type observer = (world: World) => PatchedWorld;
