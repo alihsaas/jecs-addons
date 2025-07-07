50f297404ab7b8780808d134557565547ccc4f82
Jun Jie Huang <1juhuang2@gmail.com>
2025-07-06 19:22:12 -0700

    fix: update types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import "./jecs";
 import obs from "./observers";
-declare namespace JecsAddons {
-  type PatchedWorld = ReturnType<typeof obs>;
-  const observers: typeof obs;
-}
 
-export = JecsAddons;
+export = obs;
--- src/observers.d.ts
+++ src/observers.d.ts
@@ -1,14 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Entity, Id, Query, World } from "@rbxts/jecs";
 
-interface PatchedWorld extends World {
-    added<T>(this: PatchedWorld, id: Id<T>, callback: (e: Entity, id: Id, value: T) => void): () => void;
-    removed<T>(this: PatchedWorld, id: Id<T>, callback: (e: Entity, id: Id) => void): () => void;
-    changed<T>(this: PatchedWorld, id: Id<T>, callback: (e: Entity, id: Id, value: T) => void): () => void
-    observer<T extends unknown[]>(this: PatchedWorld, query: Query<T>, callback?: (e: Entity) => void): void & IterableFunction<T>;
-    monitor(this: PatchedWorld, query: Query<unknown[]>, callback: (e: Entity, id: Id) => void): void;
+interface ObservableWorld extends World {
+    added<T>(this: ObservableWorld, id: Id<T>, callback: (e: Entity, id: Id, value: T) => void): () => void;
+    removed<T>(this: ObservableWorld, id: Id<T>, callback: (e: Entity, id: Id) => void): () => void;
+    changed<T>(this: ObservableWorld, id: Id<T>, callback: (e: Entity, id: Id, value: T) => void): () => void
+    observer<T extends unknown[]>(this: ObservableWorld, query: Query<T>, callback?: (e: Entity) => void): void & IterableFunction<T>;
+    monitor(this: ObservableWorld, query: Query<unknown[]>, callback: (e: Entity, id: Id) => void): void;
 }
 
-type Observers = (world: World) => PatchedWorld;
-declare const observers: Observers
-export = observers;
+type Observers = (world: World) => ObservableWorld;
+
+declare namespace Observer {
+    type PatchedWorld = ObservableWorld;
+    const observers: Observers;
+}
+
+export = Observer
