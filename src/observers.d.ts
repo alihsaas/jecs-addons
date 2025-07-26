d41d9f23b4591350453891f5fbdce6bcca867d6a
Jun Jie Huang <1juhuang2@gmail.com>
2025-07-25 20:24:53 -0700

    fix: add component arg

--- src/observers.d.ts
+++ src/observers.d.ts
@@ -7,19 +7,19 @@ interface ObservableWorld extends World {
      * @param callback - The function to call when an entity is added.
      * @returns A function to unregister the callback.
      */
-    added<T>(this: ObservableWorld, callback:<e>(e: Entity<e>, id: Id<T>, value?: T) => void): () => void;
+    added<T>(this: ObservableWorld, component: Id<T>, callback: (e: Entity, id: Id<T>, value?: T) => void): () => void;
     /**
      * Registers a callback for when an entity with the specified component is removed.
      * @param callback - The function to call when an entity is removed.
      * @returns A function to unregister the callback.
      */
-    removed<T>(this: ObservableWorld, callback:<e>(e: Entity<e>, id: Id<T>, value?: T) => void): () => void;
+    removed<T>(this: ObservableWorld, component: Id<T>, callback: (e: Entity, id: Id<T>) => void): () => void;
     /**
      * Registers a callback for when an entity with the specified component is changed.
      * @param callback - The function to call when an entity is changed.
      * @returns A function to unregister the callback.
      */
-    changed<T>(this: ObservableWorld, callback:<e>(e: Entity<e>, id: Id<T>, value?: T) => void): () => void;
+    changed<T>(this: ObservableWorld, component: Id<T>, callback: (e: Entity, id: Id<T>, value?: T) => void): () => void;
     observer<T extends unknown[]>(this: ObservableWorld, query: Query<T>, callback?: (e: Entity, c: Id, value?: any) => void): () => () => Entity;
     monitor<T extends unknown[]>(this: ObservableWorld, query: Query<T>, callback?: (e: Entity, c: Id, value?: any) => void): () => () => Entity;
 }
