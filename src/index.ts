8c638b8dbf53427d426b9d3fcc4a75e01f5b4686
alihsaas <alihsaas2@gmail.com>
2025-08-02 09:07:01 +0300

    update to latest observer and bump jecs

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
 import "./jecs";
-import observer from "./observers";
-
-namespace JecsAddons {
-  /**
-   * The observer world returned when constructing the observer.
-   */
-  export type ObserverWorld = ReturnType<typeof observer>;
-  /**
-   * Adds the observer functionality to the Jecs namespace.
-   * This allows you to use the observer pattern for world updates.
-   */
-  export const observer_add: typeof observer = observer;
-}
-
-export = JecsAddons;
+export * from "./observers";
--- src/observers.d.ts
+++ src/observers.d.ts
@@ -1,31 +1,4 @@
-/* eslint-disable prettier/prettier */
-import type { Entity, Id, Query, World } from "@rbxts/jecs";
+import type { Entity, Id, Query } from "@rbxts/jecs";
 
-interface ObservableWorld extends World {
-    /**
-     * Registers a callback for when an entity with the specified component is added.
-     * @param callback - The function to call when an entity is added.
-     * @returns A function to unregister the callback.
-     */
-    added<T>(this: ObservableWorld, component: Id<T>, callback: (e: Entity, id: Id<T>, value: T) => void): () => void;
-    /**
-     * Registers a callback for when an entity with the specified component is removed.
-     * @param callback - The function to call when an entity is removed.
-     * @returns A function to unregister the callback.
-     */
-    removed<T>(this: ObservableWorld, component: Id<T>, callback: (e: Entity, id: Id<T>) => void): () => void;
-    /**
-     * Registers a callback for when an entity with the specified component is changed.
-     * @param callback - The function to call when an entity is changed.
-     * @returns A function to unregister the callback.
-     */
-    changed<T>(this: ObservableWorld, component: Id<T>, callback: (e: Entity, id: Id<T>, value: T) => void): () => void;
-    observer<T extends unknown[]>(this: ObservableWorld, query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): () => () => Entity;
-    monitor<T extends unknown[]>(this: ObservableWorld, query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): () => () => Entity;
-}
-
-type Observers = (world: World) => ObservableWorld;
-
-declare const observers: Observers;
-
-export = observers;
+export declare function observer<T extends unknown[]>(query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): () => () => Entity;
+export declare function monitor<T extends unknown[]>(query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): () => () => Entity;
