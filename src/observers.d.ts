1ce99bc732ec1c1c9b9c485ed2f396e0136115b5
alihsaas <40891677+alihsaas@users.noreply.github.com>
2025-08-02 16:31:59 +0300

    fix return type of observers

--- src/observers.d.ts
+++ src/observers.d.ts
@@ -1,4 +1,10 @@
-import type { Entity, Id, Query } from "@rbxts/jecs";
+import type { Entity, Id, Query, Iter } from "@rbxts/jecs";
 
-export declare function observer<T extends unknown[]>(query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): () => () => Entity;
-export declare function monitor<T extends unknown[]>(query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): () => () => Entity;
+export type Observer<T extends unknown[]> = {
+    iter: Iter<T>,
+    entities: Entity[],
+    disconnect: () => void,
+} & Iter<T>;
+
+export declare function observer<T extends unknown[]>(query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): Observer<T>;
+export declare function monitor<T extends unknown[]>(query: Query<T>, callback?: (e: Entity, c: Id, value: any) => void): Observer<T>;
